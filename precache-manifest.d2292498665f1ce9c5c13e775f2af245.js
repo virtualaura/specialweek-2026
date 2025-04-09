a48d2366a8f652d18437cc4a0b5cf54d346d018e
virtualaura <20338026+virtualaura@users.noreply.github.com>
2025-04-09 17:27:44 +0200

    Updates

---
+++ precache-manifest.d2292498665f1ce9c5c13e775f2af245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875bdd0c13230c0b31fa7792c5f70fc9",
+    "url": "/specialweek-2025/index.html"
+  },
+  {
+    "revision": "9586cd0f47472bb0bdf6",
+    "url": "/specialweek-2025/static/css/main.bb925948.chunk.css"
+  },
+  {
+    "revision": "32734afd9c84593cbc26",
+    "url": "/specialweek-2025/static/js/2.7a7b1505.chunk.js"
+  },
+  {
+    "revision": "b91a201c3920438428ea",
+    "url": "/specialweek-2025/static/js/3.1070cadc.chunk.js"
+  },
+  {
+    "revision": "b77b68f38505a080a2a5",
+    "url": "/specialweek-2025/static/js/4.355f0e43.chunk.js"
+  },
+  {
+    "revision": "9586cd0f47472bb0bdf6",
+    "url": "/specialweek-2025/static/js/main.a0d8930e.chunk.js"
+  },
+  {
+    "revision": "dd123fd770cb92486999",
+    "url": "/specialweek-2025/static/js/runtime~main.5ecbd54f.js"
+  }
+]);
