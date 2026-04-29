287cd6d956d7390b15bf3f17f1dc936a88160d78
virtualaura <virtualaura@gmail.com>
2026-04-29 15:32:21 +0200

    edit tasks

---
+++ build/precache-manifest.ac0e7802beeafefb60ffbadf4242610c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effc63482df3845cf167a991a96b6866",
+    "url": "/specialweek-2026/index.html"
+  },
+  {
+    "revision": "791e4d9948b0a0a71e09",
+    "url": "/specialweek-2026/static/css/main.bd7a9dae.chunk.css"
+  },
+  {
+    "revision": "06f4b70f562990f1bbba",
+    "url": "/specialweek-2026/static/js/2.af649556.chunk.js"
+  },
+  {
+    "revision": "649102e1f892bab14138fc1c6c35b64e",
+    "url": "/specialweek-2026/static/js/2.af649556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22efca625489c381feb",
+    "url": "/specialweek-2026/static/js/3.d5a4d77b.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/specialweek-2026/static/js/3.d5a4d77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2ca10112598b65a1a1",
+    "url": "/specialweek-2026/static/js/4.e33eb713.chunk.js"
+  },
+  {
+    "revision": "a94b351c1d75449559014e409020f086",
+    "url": "/specialweek-2026/static/js/4.e33eb713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791e4d9948b0a0a71e09",
+    "url": "/specialweek-2026/static/js/main.6221e766.chunk.js"
+  },
+  {
+    "revision": "cc3d5c1c150ffb49a98e",
+    "url": "/specialweek-2026/static/js/runtime-main.c2f04089.js"
+  }
+]);
